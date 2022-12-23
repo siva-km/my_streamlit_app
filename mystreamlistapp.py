eb8d13a1c4fe0512d3f2da6bebe63c2bb90e94dc
siva-km <121122333+siva-km@users.noreply.github.com>
2022-12-23 12:44:01 +0530

    Update mystreamlistapp.py
    testing with py module for snowflake

--- mystreamlistapp.py
+++ mystreamlistapp.py
@@ -42,3 +42,12 @@ fruityvoice_resp_var = req.get("https://fruityvice.com/api/fruit/"+fruit_choice)
 
 fruityvoice_norm_var = pd.json_normalize(fruityvoice_resp_var.json())
 slit.dataframe(fruityvoice_norm_var)
+
+#testing with python module
+import snowflake.connector
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
